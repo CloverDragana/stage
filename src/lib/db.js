a7408c4261dee07d2b3e7d88d1f64cf526f37055
CloverDragana <blaacklist@live.com>
2025-02-17 16:42:17 +0000

    Vercel database host testing

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,24 @@
+// import { Pool } from 'pg';
+
+// const postgresConnection = new Pool({
+//     user: process.env.NEXT_PUBLIC_PGUSER,
+//     host: process.env.NEXT_PUBLIC_PGHOST,
+//     database: process.env.NEXT_PUBLIC_PGDATABASE,
+//     password: process.env.NEXT_PUBLIC_PGPASSWORD,
+//     port: process.env.NEXT_PUBLIC_PGPORT,
+// });
+
+// export default postgresConnection;
+
+
 import { Pool } from 'pg';
 
 const postgresConnection = new Pool({
-    user: process.env.NEXT_PUBLIC_PGUSER,
-    host: process.env.NEXT_PUBLIC_PGHOST,
-    database: process.env.NEXT_PUBLIC_PGDATABASE,
-    password: process.env.NEXT_PUBLIC_PGPASSWORD,
-    port: process.env.NEXT_PUBLIC_PGPORT,
+    connectionString: "postgres://neondb_owner:npg_vVf9torjkU6O@ep-sweet-cake-a2l3hl14-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require",
+    ssl: {
+        require: true,
+        rejectUnauthorized: true
+    }
 });
 
 export default postgresConnection;
